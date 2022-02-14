c60f8d5809b2dd52c65361da6d582400b908c93a
HasaneenDev <33404473+HasaneenDev@users.noreply.github.com>
2022-02-15 02:28:30 +0300

    add black list of employees

--- src/app/views/dashboard/upload-file/upload-file.component.ts
+++ src/app/views/dashboard/upload-file/upload-file.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrService } from 'ngx-toastr';
-import { UploadFileService } from 'src/app/shared/services/upload-file.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ToastrService } from "ngx-toastr";
+import { UploadFileService } from "src/app/shared/services/upload-file.service";
 
 @Component({
-  selector: 'app-upload-file',
-  templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.scss']
+  selector: "app-upload-file",
+  templateUrl: "./upload-file.component.html",
+  styleUrls: ["./upload-file.component.scss"],
 })
 export class UploadFileComponent implements OnInit {
   hodidaysSelected: any[] = [];
@@ -15,70 +15,80 @@ export class UploadFileComponent implements OnInit {
   fileData: any;
   employees: any;
   nowDate = new Date(Date.now());
+  blockedEployees: any = [];
   constructor(
     private fb: FormBuilder,
     private modalService: NgbModal,
     private uploadFileService: UploadFileService,
     private toastr: ToastrService
-    
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-    console.log('====================================');
-    console.log(this.nowDate.getMonth() + 1);
-    console.log('====================================');
     this.uploadFileForm = this.fb.group({
       month: [this.nowDate.getMonth() + 1, Validators.required],
       year: [this.nowDate.getFullYear(), Validators.required],
-      startTime: ['07:45', Validators.required],
-      endTime: ['14:20', Validators.required],
-      file: ['', Validators.required],
-
-    })
-  }
-  open(holidaysModel){
-    this.modalService.open(holidaysModel, { ariaLabelledBy: 'modal-basic-title' })
-    .result.then((result) => {
-      console.log(result);
-    }, (reason) => {
-      console.log('Err!', reason);
+      startTime: ["07:45", Validators.required],
+      endTime: ["14:20", Validators.required],
+      file: ["", Validators.required],
     });
   }
-  addHoliday(holiday){
+  open(holidaysModel) {
+    this.modalService
+      .open(holidaysModel, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        (result) => {
+          console.log(result);
+        },
+        (reason) => {
+          console.log("Err!", reason);
+        }
+      );
+  }
+  addHoliday(holiday) {
     this.hodidaysSelected.push(holiday);
   }
-  deleteDayFromHoliday(i){
-    this.hodidaysSelected.splice(i,1)
+  deleteDayFromHoliday(i) {
+    this.hodidaysSelected.splice(i, 1);
   }
-  uploadFileBtn(e){
+  addBlockedEmployee(accountNumber) {
+    this.blockedEployees.push(accountNumber);
+  }
+
+  deleteBlockedEployees(i) {
+    this.blockedEployees.splice(i, 1);
+  }
+  uploadFileBtn(e) {
     console.log(e.target.files[0]);
-    this.fileData = e.target.files[0]
-    
+    this.fileData = e.target.files[0];
   }
-  
-  uploadFile(){
-    const fromData = new FormData()
+
+  uploadFile() {
+    const fromData = new FormData();
     this.hodidaysSelected.forEach((day) => {
-      fromData.append('Holidays', day)
-    })
-    fromData.append('month',this.uploadFileForm.get('month').value)
-    fromData.append('year',this.uploadFileForm.get('year').value)
-    fromData.append('startTime',this.uploadFileForm.get('startTime').value)
-    fromData.append('endTime',this.uploadFileForm.get('endTime').value)
-    fromData.append('file', this.fileData)
+      fromData.append("Holidays", day);
+    }),
+    this.blockedEployees.forEach((acoount) => {
+      fromData.append("EmployeeIds", acoount);
+    }),
+    fromData.append("month", this.uploadFileForm.get("month").value);
+    fromData.append("year", this.uploadFileForm.get("year").value);
+    fromData.append("startTime", this.uploadFileForm.get("startTime").value);
+    fromData.append("endTime", this.uploadFileForm.get("endTime").value);
+    fromData.append("file", this.fileData);
     console.log(fromData);
-    this.uploadFileService.uploadFile(fromData).subscribe((res:any) => {
-      console.log(res);
-      this.employees = res.data
-      this.toastr.success("تم رفع الملف")
-    }, (err:any) => {
-      if(err.error.errorCode == 1){
-        this.toastr.error("مرفوع فايل مسبقاً","لم يتم رفع الملف")
+    this.uploadFileService.uploadFile(fromData).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.employees = res.data;
+        this.toastr.success("تم رفع الملف");
+      },
+      (err: any) => {
+        if (err.error.errorCode == 1) {
+          this.toastr.error("مرفوع فايل مسبقاً", "لم يتم رفع الملف");
+        } else {
+          this.toastr.error("لم يتم رفع الملف");
+        }
       }
-      else{
-        this.toastr.error("لم يتم رفع الملف")
-      }
-    })
+    );
   }
-  
 }
