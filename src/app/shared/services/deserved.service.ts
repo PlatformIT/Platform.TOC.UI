debb904a1d45a9ed1bc05200ae81147822ff20f8
HasaneenDev <33404473+HasaneenDev@users.noreply.github.com>
2022-03-17 16:54:41 +0300

    add not deserved to contract and assignment

--- src/app/shared/services/deserved.service.ts
+++ src/app/shared/services/deserved.service.ts
@@ -1,24 +1,34 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DeservedService {
-
   baseUrl = "https://toc-api.theitplatform.app/api/";
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
+
   uploadFile(data) {
     return this.http.post(this.baseUrl + "DeserveOvertime/uploadFile", data);
   }
   getAll() {
     return this.http.get(this.baseUrl + "DeserveOvertime/GetAll");
   }
-  getAllDoseNoteDesreve(data) {
-    return this.http.get(this.baseUrl + `Employee/GetDoesNotDeserve?month=${data.month}&year=${data.year}`);
-  }
   deleteAll() {
     return this.http.delete(this.baseUrl + "DeserveOvertime/deleteAll");
+  }
+
+  getAllDoseNoteDesreve(data) {
+   if (localStorage.getItem("roles") == '"contract"') {
+      return this.http.get(
+        this.baseUrl +
+          `Contracts/GetDoesNotDeserve?month=${data.month}&year=${data.year}`
+      );
+    } else if (localStorage.getItem("roles") == '"assignment"') {
+      return this.http.get(
+        this.baseUrl +
+          `Assignments/GetDoesNotDeserve?month=${data.month}&year=${data.year}`
+      );
+    }
   }
 }
